1a984a1f0954dcf70b16607f326f7095417cf4f7
bruninhamsc <sunnycarvalho96@gmail.com>
2026-09-25 09:49:27 -0300

    JS

---
+++ AULA03/conceito_array.js
@@ -0,0 +1,19 @@
+// Array
+// Um array é uma lista que pode armazenar vários valores.
+
+let frutas = ["Maça" , "Banana" , "Uva"];
+// let frutas = ["0" , "1" , "2"]
+console.log(frutas[1])
+
+// podemos adicionar itens!!, sem alterar o array diretamente.
+frutas.push("laranja"); //adiciona ao final
+console.log(frutas[3]);
+frutas.pop(); // Remove do final (no nosso caso a laranja [3])
+console.log(frutas);
+
+// - Crie um array chamado 'animais' e adicione três animais.
+// - Exiba o primeiro e o ultimo e o último no console.
+
+let animais = ["Lontra" , "Cachorro" , "Furão"];
+console.log(frutas);
+
---
+++ AULA03/conceito_objeto.js
@@ -0,0 +1,41 @@
+// Object (Objetos)
+// Um objeto agrupa várias informações dentro de uma mesma variável,usando chave e valor
+
+let pessoa = {
+    nome: "Ana",
+    idade: 16,
+    cidade: "São Paulo"
+};
+
+let pessoa = {
+    nome: "Brenda",
+    idade: 16,
+    cidade: "São Paulo"
+};
+
+//console.log(pessoa.nome);
+//console.log(pessoa.idade);
+//console.log(pessoa.cidade);
+
+// - Crie um objeto chamado 'carro' com as propriedades 'marca', 'modelo' e 'ano'.
+// Exiba cada propriedade no console.
+
+let carro = {
+    marca: "Porsche",
+    modelo: "Taycan GTS",
+    ano: 2025
+};
+
+console.log(carro.marca);
+console.log(carro.modelo);
+console.log(carro.ano);
+
+let moto = {
+    marca: "Yamaha",
+    modelo:"Fazer 250 ABS",
+    ano: 2020
+};
+
+console.log(moto.marca);
+console.log(moto.modelo);
+console.log(moto.ano);
